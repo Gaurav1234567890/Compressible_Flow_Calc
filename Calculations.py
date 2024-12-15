af01a3b61ade8c0806b454b6488281e3c864e8a5
Gaurav1234567890 <38824354+Gaurav1234567890@users.noreply.github.com>
2024-12-15 14:57:30 -0600

    Create Calculations.py

---
+++ Calculations.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tkinter
+
+
+'''IDEAL GAS ASSUMPTION; gas is air'''
+
+
+###INPUTS
+
+
+
+
+
+#constants
+R = 287 #J/kgK
+gamma = 1.4
+
+### Normal Shock Relations
